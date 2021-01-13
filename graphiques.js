627e40b6c1d0a5f420b107a1d2e3467b81071b35
Olivier Ross <olivier.ross@usherbrooke.ca>
2021-01-13 14:11:20 -0500

    pre-alpha avancement

--- graphiques.js
+++ graphiques.js
@@ -3,13 +3,15 @@ var _ = require('lodash');
 
 window.onload = async function () {
   let donnees = await importer();
-  console.log(donnees);
-  donnees = _.groupBy(_.flatten(donnees.values), function (x) { return x; })
-  console.log(donnees);
+  let entetes = donnees.slice(0, 1);
+  let valeurs = donnees.slice(1);
+
+  let avancement_objectifs = _.groupBy(_.flatten(valeurs.map(x => x.slice(3))));
+  console.log(avancement_objectifs);
 }
 
 async function importer () {
   let response = await fetch("https://sheets.googleapis.com/v4/spreadsheets/1yTITCMmTejSxkloLJP1auk6pAcA1vtpQ8V6IZkqpFxg/values/'Phase 1'!A:L?key=AIzaSyCDbtmNllO1DY1lkrmBTjCHx_0g5Mm_gTw");
   if(!response.ok) throw "Impossible d'accéder aux données";
-  return response.json();
+  return (await response.json()).values;
 } 
